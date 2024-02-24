21f3e509af2ab13516c0aa445a252e02178f9654
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-02-25 02:59:23 +0900

    add - extension 추가

---
+++ tip-calculator/tip-calculator/config/ThemeColor.swift
@@ -0,0 +1,16 @@
+//
+//  ThemeColor.swift
+//  tip-calculator
+//
+//  Created by 이명진 on 2/25/24.
+//
+
+import UIKit
+
+struct ThemeColor {
+    static let bg = UIColor(hexString: "F5F3F4")
+    static let primary = UIColor(hexString: "1CC9BE")
+    static let secondary = UIColor.systemOrange
+    static let text = UIColor(hexString: "000000")
+    static let separator = UIColor(hexString: "CCCCCC")
+}
---
+++ tip-calculator/tip-calculator/config/ThemeFont.swift
@@ -0,0 +1,23 @@
+//
+//  ThemeFont.swift
+//  tip-calculator
+//
+//  Created by 이명진 on 2/25/24.
+//
+
+import UIKit
+
+struct ThemeFont {
+    
+    static func regular(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Regular", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func bold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Bold", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func demiBold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-DemiBold", size: size) ?? .systemFont(ofSize: size)
+    }
+}
