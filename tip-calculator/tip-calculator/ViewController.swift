45ea45fb5c2b5a0b753d0b793154d144f2e12b9b
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-02-24 23:11:19 +0900

    feat - 코드 베이스 새 프로젝트 추가+세팅

---
+++ tip-calculator/tip-calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip-calculator
+//
+//  Created by 이명진 on 2/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
