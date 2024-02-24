6e81a145cda91cca5d363dcdb3b3c7d0b22d1256
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-02-25 02:59:10 +0900

    feat - design logoView

--- tip-calculator/tip-calculator/CalculatorVC.swift
+++ tip-calculator/tip-calculator/CalculatorVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class CalculatorVC: UIViewController {
     
@@ -15,13 +16,60 @@ class CalculatorVC: UIViewController {
     private let tipInputView = TipInputView()
     private let splitInputView = SplitInputView()
     
+    private lazy var vStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            logoView,
+            resultView,
+            billInputView,
+            tipInputView,
+            splitInputView,
+            UIView()
+        ])
+        
+        stackView.axis = .vertical
+        stackView.spacing = 36
+        return stackView
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .yellow
+        setLayout()
     }
-
-
+    
+    private func setLayout() {
+        
+        view.addSubview(vStackView)
+        view.backgroundColor = ThemeColor.bg
+        
+        vStackView.snp.makeConstraints {
+            $0.leading.equalTo(view.snp.leadingMargin).offset(16)
+            $0.trailing.equalTo(view.snp.trailingMargin).offset(-16)
+            $0.bottom.equalTo(view.snp.bottomMargin).offset(-16)
+            $0.top.equalTo(view.snp.topMargin).offset(16)
+        }
+        
+        logoView.snp.makeConstraints {
+            $0.height.equalTo(48)
+        }
+        
+        resultView.snp.makeConstraints {
+            $0.height.equalTo(224)
+        }
+        
+        billInputView.snp.makeConstraints {
+            $0.height.equalTo(56)
+        }
+        
+        tipInputView.snp.makeConstraints {
+            $0.height.equalTo(56+56+16)
+        }
+        
+        splitInputView.snp.makeConstraints {
+            $0.height.equalTo(56)
+        }
+        
+    }
 }
 
--- tip-calculator/tip-calculator/View/LogoView.swift
+++ tip-calculator/tip-calculator/View/LogoView.swift
@@ -9,6 +9,52 @@ import UIKit
 
 class LogoView: UIView {
     
+    private let imageView: UIImageView = {
+        let view = UIImageView(image: .icCalculatorBW)
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
+    
+    private let topLabel: UILabel = {
+        let label = UILabel()
+        let text = NSMutableAttributedString(
+            string: "Mr Tip",
+            attributes: [.font: ThemeFont.demiBold(ofSize: 16)])
+        text.addAttributes([.font: ThemeFont.bold(ofSize: 24)], range: NSMakeRange(3, 3))
+        label.attributedText = text
+        return label
+    }()
+    
+    private let bottomLabel: UILabel = {
+        LabelFactory.build(
+            text: "Calculator",
+            font: ThemeFont.demiBold(ofSize: 20),
+            textAlignment: .left
+        )
+    }()
+    
+    private lazy var vStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [
+            topLabel,
+            bottomLabel
+        ])
+        view.axis = .vertical
+        view.spacing = -4
+        return view
+    }()
+    
+    
+    private lazy var hStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [
+            imageView, vStackView
+        ])
+        
+        view.axis = .horizontal
+        view.spacing = 8
+        view.alignment = .center
+        return view
+    }()
+    
     init() {
         super.init(frame: .zero)
         setLayout()
@@ -20,6 +66,14 @@ class LogoView: UIView {
     
     
     private func setLayout() {
-        backgroundColor = .red
+        addSubview(hStackView)
+        hStackView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.centerX.equalToSuperview()
+        }
+        
+        imageView.snp.makeConstraints {
+            $0.height.equalTo(imageView.snp.width)
+        }
     }
 }
